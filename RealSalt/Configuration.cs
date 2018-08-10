c07bd077783a26c21a00d3e0f0f7743505171192
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2018-08-10 18:24:42 +0200

    Implemented Bet $10 on Blue Strategy

---
+++ RealSalt/Configuration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace RealSalt
+{
+    class Configuration
+    {
+        public static string DataPath = "Data/";
+        public static string DataFileName = "Config.json";
+        public static string FullPath => DataPath + DataFileName;
+
+        public string SaltyAccount;
+        public string SaltyAccountPassword;
+
+        public Configuration()
+        {
+            SaltyAccount = "Salty Account Name";
+            SaltyAccountPassword = "ASuperSecretPasswordNobodyKnows";
+        }
+
+        public void Save()
+        {
+            var data = JsonConvert.SerializeObject(this, Formatting.Indented);
+
+            if (!Directory.Exists(DataPath))
+            {
+                Directory.CreateDirectory(DataPath);
+            }
+
+            try
+            {
+                File.WriteAllText(FullPath, data);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Could not create Config.json");
+            }
+
+        }
+
+        public static Configuration Load()
+        {
+            var data = File.ReadAllText(FullPath);
+            return JsonConvert.DeserializeObject<Configuration>(data);
+        }
+    }
+}
--- RealSalt/Program.cs
+++ RealSalt/Program.cs
@@ -1,18 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using ChromiumConsole;
+using ChromiumConsole.EventArguments;
+using Serilog;
 
 namespace RealSalt
 {
     class Program
     {
+        private static SaltyConsole _saltyBetConsole;
+        private static Configuration _saltyConfiguration;
+
         static void Main(string[] args)
         {            
-            var console = new SaltyConsole();
-            console.Start("", "");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()                
+                .WriteTo.Console()
+                .CreateLogger();
+
+            LoadConfig();
+
+            _saltyBetConsole = new SaltyConsole();
+            
+            _saltyBetConsole.LoginSuccess += SaltyBetConsoleOnLoginSuccess;
+            _saltyBetConsole.MatchStart += ConsoleOnMatchStart;
+            _saltyBetConsole.MatchEnded += SaltyBetConsoleOnMatchEnded;
+
+            _saltyBetConsole.Start(_saltyConfiguration.SaltyAccount, _saltyConfiguration.SaltyAccountPassword);
+        }
+
+
+        private static void LoadConfig()
+        {
+            if (File.Exists(Configuration.FullPath))
+            {
+                _saltyConfiguration = Configuration.Load();
+            }
+            else
+            {
+                _saltyConfiguration = new Configuration();
+                _saltyConfiguration.Save();
+            }
+        }
+
+
+        private static void SaltyBetConsoleOnLoginSuccess(object sender, EventArgs e)
+        {           
+            Log.Information($"Logged in to SaltyBet.");
+        }
+
+        private static void ConsoleOnMatchStart(object sender, EventArgs eventArgs)
+        {
+            var matchStartArgs = (MatchStartEventArgs) eventArgs;
+            
+           _saltyBetConsole.PlaceBet(SaltyConsole.Players.RedPlayer,10);
+
+            Log.Information($"Match Started : {matchStartArgs.RedPlayer} vs {matchStartArgs.BluePlayer}. Betting 10$ on {matchStartArgs.RedPlayer}.");
+        }
+
+        private static void SaltyBetConsoleOnMatchEnded(object sender, EventArgs eventArgs)
+        {
+            var matchEndArgs = (MatchEndEventArgs) eventArgs;
+
+            Log.Information($"Match Ended: {matchEndArgs.WinningPlayerName} won. Balance {matchEndArgs.Salt}[{matchEndArgs.SaltBalanceChange}].");
         }
     }
 }
