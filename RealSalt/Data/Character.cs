2ee9390bbcc8539360e45e7064080ad4687d4d2a
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2018-08-11 01:03:55 +0200

    Cleaned  usings.

--- RealSalt/Data/Character.cs
+++ RealSalt/Data/Character.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RealSalt.Data
+﻿namespace RealSalt.Data
 {
     public class Character
     {
